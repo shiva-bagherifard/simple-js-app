9dcfbd92f5d04462c21eb3dd18d5f02da87bc877
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-03-06 16:22:32 +0100

    updated scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: "Charmander",
+        height: 6,
+        types: ['fire']
+    },
+    {
+        name: "Squirtle",
+        height: 5,
+        types: ['water']
+    }
+];
