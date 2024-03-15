f73ba143dcf9a8ed5b4bdf6f58e2a07aeb6b6b54
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-03-15 12:24:50 +0100

    updated index.html,css and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -72,3 +72,56 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListItem(pokemon);
     });
 });
+
+//modal here
+
+(function () {
+    let modalContainer = document.querySelector(".modal-container");
+  
+    function showModal(pokemon) {
+      let modal = document.createElement("div");
+      modal.classList.add("modal");
+  
+      let closeButtonElement = document.createElement("button");
+      closeButtonElement.classList.add("modal-close");
+      closeButtonElement.innerText = "Close";
+      closeButtonElement.addEventListener("click", hideModal);
+  
+      let titleElement = document.createElement("h1");
+      titleElement.innerText = pokemon.name;
+  
+      let contentElement = document.createElement("p");
+      contentElement.innerText = pokemon.height;
+  
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modalContainer.appendChild(modal);
+      modalContainer.classList.add("is-visible");
+    }
+  
+    let dialogPromiseReject;
+  
+    function hideModal() {
+      let modalContainer = document.querySelector(".modal-container");
+      modalContainer.classList.remove("is-visible");
+  
+      if (dialogPromiseReject) {
+        dialogPromiseReject();
+        dialogPromiseReject = null;
+      }
+    }
+  
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+        hideModal();
+      }
+    });
+  
+    modalContainer.addEventListener("click", (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  })();
